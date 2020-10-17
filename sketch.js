690d5bb756c913f419b0d6c66a3fae8bbe3cac4a
baddystar12 <72180686+baddystar12@users.noreply.github.com>
2020-10-17 15:48:04 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
 var bullet, wall;
-var speed, weight;
 var thickness;
 var bulletWeight, bulletSpeed;
 
 function setup() {
   createCanvas(1600,400);
-  speed = random(223, 321);
-  weight = random(30, 52);
+  bulletSpeed = random(223, 321);
+  bulletWeight = random(30, 52);
   thickness = random(22, 83);
   bullet =createSprite(50, 200, 10, 50);
   wall = createSprite(1200, 60, thickness, height/2);
@@ -16,8 +15,8 @@ function setup() {
 function draw() {
   background(255,255,255);  
   wall.shapeColor = (80, 80, 80);
-  bullet.shapeColor = "white";
-  bullet.velocityX = speed;
+  bullet.shapeColor = "purple";
+  bullet.velocityX = bulletSpeed;
    if(isTouching(bullet, wall)){
     bullet.velocityX = 0;
     var damage = 0.5*bulletWeight*bulletSpeed*bulletSpeed/wall.thickness*wall.thickness* wall.thickness;
